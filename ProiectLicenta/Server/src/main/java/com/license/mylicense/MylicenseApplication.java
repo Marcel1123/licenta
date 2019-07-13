b1c8625c3017c96523643b428ca838bf101e9eb1
Marcel1123 <48123341+Marcel1123@users.noreply.github.com>
2019-07-13 19:41:44 +0300

    Add files via upload

---
+++ ProiectLicenta/Server/src/main/java/com/license/mylicense/MylicenseApplication.java
@@ -0,0 +1,13 @@
+package com.license.mylicense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylicenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MylicenseApplication.class, args);
+    }
+
+}
---
+++ ProiectLicenta/Server/src/test/java/com/license/mylicense/MylicenseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.license.mylicense;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MylicenseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
